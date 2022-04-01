8435fd66639868725bf5b615c89870301aaa3ee0
Tim Jongenelen <p.j.jongenelen@student.tue.nl>
2022-04-01 11:39:30 +0200

    New version of annotation software

--- src/2_exploration and annotation/tweet_sentiment_annotator.py
+++ src/2_exploration and annotation/tweet_sentiment_annotator.py
@@ -1,30 +1,36 @@
 import pandas as pd
+import random
 
 ROOT = "C:/Users/timjo/python_projects/footballmsc"
 
+
+def print_bars():
+    print("-------------------------------------------------------")
+    print("-----------------------------------------")
+    print("-------------------------")
+    print("---------")
+
+
+# load data and get list of tweets that still have to be annotated
 df = pd.read_csv(f"{ROOT}/data/only_tweets.csv", index_col = [0])
+todo = df.index[df.annotation == 100].tolist()
 
-unannotated = df[df.annotation == 0].copy()
 
-indices = []
-annotations = []
-
-for index, row in unannotated.iterrows():
-    print(row.content)
+for _ in todo:
+    # get a random tweet to annotate
+    index = random.sample(todo, 1)
+    print(df.iloc[index]['content'].values[0])
     annotation = input()
+
     if annotation == "!":
+        # stop annotating and save the file
         break
 
-    indices.append(index)
-    annotations.append(annotation)
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
-    print("------------------------------------------------------------")
+    elif annotation in [-2, -1, 0, 1]:
+        # correct encoding, no typo
+        df.loc[index,'annotation'] = annotation
+        print_bars()
 
-annotations_df = pd.DataFrame({"index": indices, "annotation": annotations})
-df = annotations_df.combine_first(df)
 
-df.to_csv(f"{ROOT}/data/only_tweets.csv")
+# save to csv
+df.to_csv(f'{ROOT}/data/only_tweets.csv')
